99a85f3f4b5b879bec08c0c047459bd99520b743
haewonwon <khcw1029@daum.net>
2026-02-08 16:23:13 +0900

    Feat: 페이지 파일 추가 및 라우팅 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
-import { ThemeProvider } from 'styled-components';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
 import { useMediaQuery } from 'react-responsive';
 import original from 'react95/dist/themes/original';
 import { GlobalStyles } from './styles/globalTheme';
 import { breakpoints } from './styles/breakpoints';
+import {
+  LandingPage,
+  SignUpPage,
+  LoginPage,
+  TestPage,
+  MyPage,
+  AdminPage,
+  SettingPage,
+} from './pages';
 import DesktopBlocker from './components/shared/DesktopBlocker';
 
 const App = () => {
@@ -13,12 +22,20 @@ const App = () => {
     <>
       <GlobalStyles />
       <ThemeProvider theme={original}>
+        {/* 1024px 이상부터 차단 */}
         {isDesktop ? (
           <DesktopBlocker />
         ) : (
           <Router>
             <Routes>
-              <Route path="/" element={<div>Home Page (Mobile/Tablet Only)</div>} />
+              <Route path="/" element={<LandingPage />} />
+              <Route path="/signup" element={<SignUpPage />} />
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/test" element={<TestPage />} />
+              <Route path="/my" element={<MyPage />} />
+              
+              <Route path="/admin" element={<AdminPage />} />
+              <Route path="/admin/setting" element={<SettingPage />} />
             </Routes>
           </Router>
         )}
---
+++ src/pages/index.ts
@@ -0,0 +1,8 @@
+export { default as LandingPage } from './user/LandingPage';
+export { default as SignUpPage } from './user/SignUpPage';
+export { default as LoginPage } from './user/LoginPage';
+export { default as TestPage } from './user/TestPage';
+export { default as MyPage } from './user/MyPage';
+export { default as AdminPage } from './admin/AdminPage';
+export { default as SettingPage } from './admin/SettingPage';
+
