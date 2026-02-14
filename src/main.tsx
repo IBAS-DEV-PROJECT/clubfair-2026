f6bdd6efe46a4aa367ddc7f85d3b041ee2844af4
haewonwon <khcw1029@daum.net>
2026-02-14 17:53:37 +0900

    Rename: lib에서 libs로 폴더 이름 변경

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { QueryClientProvider } from '@tanstack/react-query'
 import App from './App.tsx'
-import { queryClient } from './lib/queryClient'
+import { queryClient } from './libs/queryClient.ts'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
