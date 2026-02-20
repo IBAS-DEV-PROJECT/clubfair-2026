9f6d2eafdb78784d1421c6237353ad83ead43207
haewonwon <khcw1029@daum.net>
2026-02-20 19:34:35 +0900

    Fix: 관리자 세팅 페이지 수정 - 이벤트 응모 컨테이너 추가 - 스크롤 가능

--- src/pages/admin/AdminSettingPage.tsx
+++ src/pages/admin/AdminSettingPage.tsx
@@ -1,16 +1,21 @@
 import styled from 'styled-components';
 import TimeSettingContainer from '../../containers/admin/TimeSettingContainer';
+import EventDrawContainer from '../../containers/admin/EventDrawContainer';
 
 const Wrapper = styled.div`
+  width: 100%;
   display: flex;
   flex-direction: column;
   gap: 24px;
+  padding-top: 40px;
+  padding-bottom: 20px;
 `;
 
 const AdminSettingPage = () => {
   return (
     <Wrapper>
       <TimeSettingContainer />
+      <EventDrawContainer />
     </Wrapper>
   );
 };
