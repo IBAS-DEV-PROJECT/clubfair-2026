abc7faa348e0a39e287d156092242b3dad5770d7
haewonwon <khcw1029@daum.net>
2026-02-20 19:05:06 +0900

    Feat: 관리자 세팅 페이지 구현

--- src/pages/admin/AdminSettingPage.tsx
+++ src/pages/admin/AdminSettingPage.tsx
@@ -1,5 +1,18 @@
+import styled from 'styled-components';
+import TimeSettingContainer from '../../containers/admin/TimeSettingContainer';
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
+`;
+
 const AdminSettingPage = () => {
-  return <>{/* TODO: 여기에 컨테이너 추가 예정 */}</>;
+  return (
+    <Wrapper>
+      <TimeSettingContainer />
+    </Wrapper>
+  );
 };
 
 export default AdminSettingPage;
