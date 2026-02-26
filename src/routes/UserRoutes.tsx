5999e471d7ed68cc16534ff044c0ab9b06551e99
haewonwon <khcw1029@daum.net>
2026-02-27 01:26:02 +0900

    Fix: 테스트 페이지 user layout 적용

--- src/routes/UserRoutes.tsx
+++ src/routes/UserRoutes.tsx
@@ -1,35 +1,33 @@
-import { useEffect } from "react";
-import { Route, Routes } from "react-router-dom";
-import ProtectedUserLayout from "../layouts/ProtectedUserLayout";
-import { LandingPage, MyPage, SignUpPage, LoginPage, TestPage } from "../pages";
-import { useUserAuthStore } from "../stores/useUserAuthStore"
+import { useEffect } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import ProtectedUserLayout from '../layouts/ProtectedUserLayout';
+import { LandingPage, MyPage, SignUpPage, LoginPage, TestPage } from '../pages';
+import { useUserAuthStore } from '../stores/useUserAuthStore';
 
 const UserRoutes = () => {
-    const init = useUserAuthStore((state) => state.init);
+  const init = useUserAuthStore((state) => state.init);
 
-    useEffect(() => {
-        init();
-    }, []);
+  useEffect(() => {
+    init();
+  }, []);
 
-    return (
-        <Routes>
-            {/* 공개 페이지 */}
-            <Route path="/" element={<LandingPage />}/>
-            <Route path="/signup" element={<SignUpPage />}/>
-            <Route path="/login" element={<LoginPage />}/>
+  return (
+    <Routes>
+      {/* 공개 페이지 */}
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/signup" element={<SignUpPage />} />
+      <Route path="/login" element={<LoginPage />} />
 
-            {/* 로그인 필요 (테스트 미완료도 접근 가능) - 개발 중일 때 아래 3줄 주석 */}
-            {/* <Route element={<ProtectedUserLayout requiredTest={false} />}>
-                <Route path="/test" element={<TestPage />} />
-            </Route> */}
-            <Route path="/test" element={<TestPage />} />
+      <Route element={<ProtectedUserLayout requiredTest={false} />}>
+        <Route path="/test" element={<TestPage />} />
+      </Route>
 
-            {/* 로그인 + 테스트 완료 필요 */}
-            <Route element={<ProtectedUserLayout />}>
-                <Route path="/my" element={<MyPage />} />
-            </Route>
-        </Routes>
-    );
+      {/* 로그인 + 테스트 완료 필요 */}
+      <Route element={<ProtectedUserLayout />}>
+        <Route path="/my" element={<MyPage />} />
+      </Route>
+    </Routes>
+  );
 };
 
 export default UserRoutes;
